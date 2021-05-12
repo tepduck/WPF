a433a36aacfe03e878a1a5a61c3c8f6f7f4755cc
tepduck <lksunaylkolodl@gmail.com>
2021-05-12 22:04:51 +0300

    Add files via upload

---
+++ NBlog01/Models/Authentication/Account.cs
@@ -0,0 +1,15 @@
+﻿using NBlog01.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBlog01.Models.Authentication
+{
+    public static class Account
+    {
+        public static Users curUser { get; set; }
+        public static bool IsLoggedIn { get; set; }
+    }
+}
